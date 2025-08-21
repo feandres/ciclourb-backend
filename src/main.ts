7181213757f02e2e1cabc53707bb16e68d60c483
Felipe Andrès <felipevsconcelos@gmail.com>
2025-08-21 20:39:58 -0300

    backend

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import express, { Express } from 'express';
 
-const server = express();
+const server: Express = express(); 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
