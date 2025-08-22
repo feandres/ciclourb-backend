418fc9e9b57bdaf4d09d385c2e96af116606b63b
Felipe Andrès <felipevsconcelos@gmail.com>
2025-08-21 22:05:15 -0300

    feat/ipv4-fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dns from 'dns';
 
 async function bootstrap() {
+  dns.setDefaultResultOrder('ipv4first');
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
